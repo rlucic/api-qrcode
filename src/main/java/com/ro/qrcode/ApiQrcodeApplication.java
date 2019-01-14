6785bead2b1a2c81f20fde891976a0480af4eb86
RGL <romulus.lucic@gmail.com>
2019-01-13 23:27:14 -0500

    inital project commit

---
+++ src/main/java/com/ro/qrcode/ApiQrcodeApplication.java
@@ -0,0 +1,14 @@
+package com.ro.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQrcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQrcodeApplication.class, args);
+	}
+
+}
+
