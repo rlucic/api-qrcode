ac5db8d55466eeba7bb9a06fa2efc59cd4f79598
RGL <rgl@email.com>
2019-02-18 21:52:55 -0500

    added v1 code

--- README.md
+++ README.md
@@ -4,5 +4,15 @@ generates QR codes
 ##Sample calls
 
 
-##Using the QR Code library
+##Version V1 of the API 
+Is using the QR Code library: com.github.kenglxn
+sample calls: 
+- http://localhost:8080/api-qrcode/image/v1/test/png
+- http://localhost:8080/api-qrcode/image/v1/test/b64
+- http://localhost:8080/api-qrcode/image/v1/generate/png
 
+##Model: 
+{
+ text: "Some sample text",
+ format: "B64"
+}
--- src/main/java/com/ro/qrcode/api/QRCodeGenV1API.java
+++ src/main/java/com/ro/qrcode/api/QRCodeGenV1API.java
@@ -3,32 +3,87 @@ package com.ro.qrcode.api;
 
 import java.io.ByteArrayOutputStream;
 import java.time.LocalDateTime;
+import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ro.qrcode.model.QRCodeModel;
 
 import io.swagger.annotations.Api;
 import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
 
+import org.apache.commons.io.IOUtils;
+ 
+/**
+ * Version V1 uses the qrgen library for generating the QR Code:com.github.kenglxn;
+ * 
+ * @author rlucic
+ *
+ */
 @Api
 @RestController
 @RequestMapping("/image/v1")
 public class QRCodeGenV1API {
 
+	@Value("${qrcode.size:200}")
+	private int imgSize;
 	
-	@GetMapping(value="/test", produces=MediaType.IMAGE_PNG_VALUE)
+	private final String textForQRCode = "Testing QR Code generation with net.glxn.qrgen.javase.QRCode library.";
+	
+	/**
+	 * Testing endpoint for generating a test QR code image. Will return the image itself.
+	 * Image type: PNG
+	 * 
+	 * @return
+	 */
+	@GetMapping(value="/test/png", produces=MediaType.IMAGE_PNG_VALUE)
 	@ResponseBody
 	public byte[] ping() {
 		
 		LocalDateTime ldt = LocalDateTime.now();
 		ByteArrayOutputStream baos = QRCode.from("Testing QR Code generation with net.glxn.qrgen.javase.QRCode library, at " +
 					ldt.toString()).
-				withSize(150, 150).to(ImageType.PNG).stream();
+				withSize(imgSize, imgSize).to(ImageType.PNG).stream();
 		
 		return baos.toByteArray();
 	}
+
+	/**
+	 * Testing endpoint for generating a test QR code image encoded as Base64.
+	 * Encoded image type: PNG.
+	 * 
+	 * @return String as Base64 encodinng of the QR code.
+	 */
+	@GetMapping(value="/test/b64", produces=MediaType.TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public String pingb64() {
+		ByteArrayOutputStream baos = QRCode.from(textForQRCode).
+				withSize(imgSize, imgSize).
+				to(ImageType.PNG).
+				stream();
+		
+		String toReturn = Base64.getEncoder().encodeToString(baos.toByteArray());
+		
+		return toReturn;
+	}
+	
+	@PostMapping(value="/generate/png", produces=MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<String> generateQRCode(QRCodeModel model) {
+		if (model==null || model.getText() == null || model.getText().isEmpty() ) {
+			return new ResponseEntity<String>("No valid mode passed", HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<String>("", HttpStatus.OK);
+	}
+	
 }
